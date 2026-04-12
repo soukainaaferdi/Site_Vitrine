983f0165918df2b3ef4881736c7a988a37504f2f
ikram-bounit <bounitikram9@gmail.com>
2026-04-12 21:03:29 +0100

    fix errors for professor

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => {
     subject: "",
     message: ""
   });
-  const [setIsSending] = useState(false);
+  const [isSending,setIsSending] = useState(false);
 
   useEffect(() => {
   
