cd9156ee856716fa9850a5c23d48d8ebc603c5da
suokaina <aferdisoukaina8@gmail.com>
2026-04-20 18:53:56 +0000

    changes json

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,32 @@
 import React from 'react';
 import '../Styles/Home.css';
-
 import { Link } from 'react-router-dom';
 const Hero = () => {
-
-
-
-
-  
-  const sectionStyle = {
-    backgroundImage: "url('/images/home.jpg')",
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    height: '100vh',
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center'
-  };
-
   return (
-    <section style={sectionStyle} className="hero-section">
+    <section className="hero-section d-flex align-items-center">
       <div className="container">
-        <div className="row">
-          <div className="col-12 col-md-8 animate-fade-in">
-            <h1 className="display-3 fw-bold mb-4">
-              Bienvenue à l’ISAG Casablanca <br /> 
-              <span className="isag-subtext">Institut Spécialisé des Arts Graphiques</span>
-            </h1>
-            <p className="lead mb-5 description">
-              Développez vos compétences en design graphique et en techniques d’impression
-            </p>
-            <div className="d-flex gap-3">
-              <button className="btn-lg rounded-pill hero-btn">
-                Contacter
-              </button>
+        <div className="row align-items-center">
+          <div className="col-lg-7 col-md-8 text-white animate-fade-in">
+            <div className="hero-content-wrapper">             
+              <h1 className="display-3 fw-bold mb-4 mt-2">
+                Bienvenue à <span className="text-highlight">l’ISAG</span> Casablanca
+              </h1>
+              <p className="lead mb-5 description">
+                Institut Spécialisé des Arts Graphiques. Développez vos compétences en design graphique, infographie et techniques d’impression de pointe.
+              </p>
+              <div className="d-flex gap-3">
+                 <a href="#formations"className="btn-lg px-5 rounded-pill hero-btn main-btn text-decoration-none " >
+                Découvrir nos filières
+               </a>
+               <a href="#contact" className="btn btn-lg px-5 rounded-pill hero-btn secondary-btn text-decoration-none d-inline-block text-white">
+                Contactez-nous
+               </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </section>
-
   );
 };
 
